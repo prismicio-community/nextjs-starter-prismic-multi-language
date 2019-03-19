f088bb83c59e70c2935f4daf1ba4510cdc520094
Raul Gonzalez <raulg42@gmail.com>
2019-03-19 16:52:51 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dev = process.env.NODE_ENV !== 'production';
+const next = require('next');
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const Prismic = require('prismic-javascript');
+const { apiEndpoint, linkResolver } = require('./prismic-configuration');
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.get('/:uid', (req, res) => {
+      const nextJsPage = '/page';
+      const queryParams = { uid: req.params.uid };
+      app.render(req, res, nextJsPage, queryParams);
+    });
+
+    server.get('/preview', (req, res) => {
+      const token = req.query.token;
+
+      Prismic.getApi(apiEndpoint, {req})
+        .then((api) => api.previewSession(token, linkResolver, '/'))
+        .then((url) => {
+          res.redirect(302, url);
+        });
+    });
+
+    server.get('*', (req, res) => handle(req, res));
+
+    server.listen(3000, err => {
+      if (err) throw err;
+      console.log('> Ready http://localhost:3000 <');
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
