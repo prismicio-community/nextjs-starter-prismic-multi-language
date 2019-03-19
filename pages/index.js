6e5914099aa8af002d81c3587bc8bb18b0c642a2
Raul Gonzalez <raulg42@gmail.com>
2019-03-19 17:05:29 +0100

    Pages

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import DefaultLayout from '../layouts'
+
+export default class extends React.Component {
+
+  render() {
+    return(
+      <DefaultLayout>
+        <div>HomePage rendered</div>
+      </DefaultLayout>
+    );
+  }
+}
---
+++ pages/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import DefaultLayout from '../layouts'
+
+export default class Page extends React.Component {
+  render() {
+    return(
+      <DefaultLayout>
+        <div>Page</div>
+      </DefaultLayout>
+    );
+  }
+}
