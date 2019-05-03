af627eaada870fc46fc300a7786ec73e735f8c13
Philip Snow <philipsnow@Philips-MacBook-Pro.local>
2019-05-03 12:26:40 +0200

    revert

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -24,7 +24,7 @@ module.exports = {
   // Additional helper function for Next/Link component
   hrefResolver: function(doc) {
     if (doc.type === 'page') {
-      return `/page?lang=${doc.lang}&page?uid=${doc.uid}`;
+      return `/page?locale=${doc.lang}&page?uid=${doc.uid}`;
     }
     if (doc.type === 'homepage') {
       return `/${doc.lang}`;
