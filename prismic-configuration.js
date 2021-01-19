7f2cfa61a338c9dc0f852bb703a89b0f91d520a2
PaulinaVPG <pvpg@live.com.mx>
2021-01-19 16:16:52 -0600

    Added apiKey variable to prismic-config

--- prismic-configuration.js
+++ prismic-configuration.js
@@ -1,3 +1,7 @@
+// If you wish to add the token in an .env, prefix it with prefix the variable with NEXT_PUBLIC_. 
+// For example: NEXT_PUBLIC_ACCESS_TOKEN. Then modify the apiKey variable below.
+const apiKey = process.env.NEXT_PUBLIC_ACCESS_TOKEN || ''
+
 module.exports = {
   // -- Prismic API endpoint
   // Determines which repository to query and fetch data from
@@ -6,7 +10,9 @@ module.exports = {
 
   // -- Access Token if the repository is not public
   // Generate a token in your dashboard and configure it here if your repository is private
-  accessToken: '',
+  // If you wish to add the token in an .env, prefix it with prefix the variable with NEXT_PUBLIC_. 
+  // For example: NEXT_PUBLIC_ACCESS_TOKEN. Then modify the apiKey variable in this file.
+  accessToken: apiKey,
   
   // -- Link resolution rules
   // Manages links to internal Prismic documents
