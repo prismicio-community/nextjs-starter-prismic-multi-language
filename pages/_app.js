c741cd5fb6b45f36c2291a35b424b909ba6661f3
Sam Littlefair <sam.littlefair@prismic.io>
2022-02-03 10:27:15 +0000

    Done

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ import 'styles/main.scss'
 
 export default function App({ Component, pageProps }) {
   return (
-    <PrismicPreview repositoryName>
+    <PrismicPreview repositoryName={repositoryName}>
       <PrismicProvider
         linkResolver={linkResolver}
         internalLinkComponent={({ href, children, ...props }) => (
--- pages/api/preview.js
+++ pages/api/preview.js
@@ -4,13 +4,8 @@ import { setPreviewData, redirectToPreviewURL } from '@prismicio/next'
 export default async (req, res) => {
   const client = createPrismicClient({ req })
   await setPreviewData({ req, res })
-  const previewURL = await client.resolvePreviewURL({
-    linkResolver,
-    defaultURL: '/',
-  })
-  res.redirect(previewURL)
 
-  // TODO: prismicio/next's redirect isn't working
+  await redirectToPreviewURL({ req, res, client, linkResolver })
 
-  // await redirectToPreviewURL({ req, res, client, linkResolver })
+  return true
 }
