5c6efc0e621112e3bbc5cfac307514e6a4e8b222
prismic <prismic@prismics-Mac-mini.local>
2022-03-11 17:36:33 +0100

    updated index file

--- .slicemachine/assets/slices/TextInfo/index.stories.js
+++ .slicemachine/assets/slices/TextInfo/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_info","items":[],"primary":{"title":[{"type":"heading1","text":"Grow out-of-the-box partnerships","spans":[]}],"description":[{"type":"paragraph","text":"Ut cupidatat aute nostrud laboris Lorem aliquip elit. Anim duis velit dolor officia ea culpa quis aliquip veniam laborum est laboris elit sunt.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_info","items":[],"primary":{"sectionTitle":[{"type":"heading1","text":"Incentivize B2C e-tailers","spans":[]}],"leftColumnText":[{"type":"paragraph","text":"Cillum irure nulla officia velit Lorem esse eiusmod esse esse Lorem aliqua voluptate aliquip aliquip. Eu cupidatat amet duis nostrud nulla aliqua.","spans":[]}],"rightColumnText":[{"type":"paragraph","text":"Ad ex eu ipsum Lorem consectetur est occaecat nulla. Commodo excepteur id reprehenderit consectetur duis sunt non duis cillum nostrud aliquip. Ex incididunt incididunt nulla commodo aliqua est sint dolor.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
